1ec14e9a06c46121432d6c9d6ada4ecd03edcc45
Svetlana <lola5_575@rambler.ru>
2019-04-08 16:58:11 +0300

    auth

---
+++ controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\controllers;
+use app\models\Users;
+use yii\web\Controller;
+class AuthController extends Controller
+{
+    public function actionSignUp(){
+        /** @var Users $model */
+        $model=\Yii::$app->auth->getModel();
+        if(\Yii::$app->request->isPost){
+            $model=\Yii::$app->auth->getModel(\Yii::$app->request->post());
+            if(\Yii::$app->auth->createUser($model)){
+                return $this->redirect(['/']);
+            }
+        }
+        return $this->render('signup',['model'=>$model]);
+    }
+    public function actionSignIn(){
+        /** @var Users $model */
+        $model=\Yii::$app->auth->getModel();
+        if(\Yii::$app->request->isPost) {
+            $model = \Yii::$app->auth->getModel(\Yii::$app->request->post());
+            if(\Yii::$app->auth->authUser($model)){
+                return $this->redirect(['/activity/create']);
+            }
+        }
+        return $this->render('signin',['model'=>$model]);
+    }
+}
